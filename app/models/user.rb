7e030c2e9e25cad8277954b9aaddbb7385464a87
Leonardo <leonardoa123@gmail.com>
2023-10-12 18:33:42 -0300

    add user to product

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true,
+        format: {
+            with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+            message: :invalid
+    }
+
+    validates :username, presence: true, uniqueness: true, 
+        length: { in: 3..15 },
+        format: { 
+            with: /\A[a-z-0-9-A-Z]+\z/,
+            messege: :invalid
+        }
+
     validates :password, length: { in:6..20 }
 
+    before_save :downcase_attributes
+
+    private
+
+    def downcase_attributes
+        self.username = username.downcase
+        self.email = email.downcase
+    end
 end
